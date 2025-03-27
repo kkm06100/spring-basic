c1971becea76a513c1bdf2d7573126b1b213b736
김경민 <kkm07133@naver.com>
2025-03-27 22:50:36 +0900

    기본 세팅

---
+++ spring-basic-test/src/main/java/org/example/springbasictest/SpringBasicTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbasictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicTestApplication.class, args);
+    }
+
+}
---
+++ spring-basic-test/src/test/java/org/example/springbasictest/SpringBasicTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbasictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
